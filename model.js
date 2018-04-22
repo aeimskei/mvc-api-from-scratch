eccd113e44abaf1b679e98a2c4095236da2569dd
Amy Boudsady <aeimskei@gmail.com>
2018-04-22 10:52:21 -0700

    Create model.js and setup database

---
+++ model.js
@@ -0,0 +1,38 @@
+const designers = [
+  {
+    id: 1,
+    name: 'Charles and Ray Eames',
+    category: 'Architecture and Furniture',
+    country: 'America'
+  },
+  {
+    id: 2,
+    name: 'Herb Lubalin',
+    category: 'Graphic Designer and Typographer',
+    country: 'America'
+  },
+  {
+    id: 3,
+    name: 'Paul Rand',
+    category: 'Graphic Designer',
+    country: 'America'
+  },
+  {
+    id: 4,
+    name: 'Saul Bass',
+    category: 'Graphic and Motion Designer',
+    country: 'America'
+  },
+  {
+    id: 5,
+    name: 'Erik Spiekermann',
+    category: 'Graphic Designer and Typographer',
+    country: 'Germany'
+  },
+  {
+    id: 6,
+    name: 'Dieter Rams',
+    category: 'Industrial Designer',
+    country: 'Germany'
+  }
+]
